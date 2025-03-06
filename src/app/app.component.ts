b09fce5874a7d925f60fc501ffad9f338d83f1fb
kuldeep chaturvedi <chaturvedikuldeep40@gmial.com>
2025-03-06 10:34:16 +0530

    [Kuldeep] Add . Added Angular Data Binding to display hello from bridgelabz with user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   imgUrl = './assets/bridgelabz.jpg';
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit() {
     this.title = "Hello from Bridgelabz.";
@@ -24,5 +25,15 @@ export class AppComponent {
   onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: any) {
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
